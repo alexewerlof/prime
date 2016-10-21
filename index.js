7fce9a3b7900558a0f407cf907f3f88be0114f27
Alex Ewerlöf <alex.ewerlof@schibsted.com>
2016-10-21 12:37:54 +0200

    put the main loop in a function

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const bignum = require('bignum');
 
 var primeCache = [];
 // Can be a numerical literal or a string containing it
-var max = bignum(10000000);
+var max = bignum(5000000);
 
-function isPrime(n) {
+function isPrime(n, primeCache) {
   var prob = n.probPrime();
   if (prob === 'maybe') {
     // if this number is not dividable to any prime currently found
@@ -14,16 +14,20 @@ function isPrime(n) {
   }
 }
 
-var counter = 0;
-for (let i = bignum(3); i.lt(max); i = i.add(2)) {
-  if (isPrime(i)) {
-    primeCache.push(i);
-  }
-  counter++;
-  if (counter > 1000) {
-    console.log(`just passed ${i.div(1000).toString()}K`);
-    counter = 0;
+function findPrimesUnder(max, primeCache) {
+  var counter = 0;
+  for (let i = bignum(3); i.lt(max); i = i.add(2)) {
+    if (isPrime(i, primeCache)) {
+      primeCache.push(i);
+    }
+    counter++;
+    if (counter > 1000000) {
+      console.log(`just passed ${i.div(1000000).toString()}M so far we have ${primeCache.length} primes...`);
+      counter = 0;
+    }
   }
 }
+
+findPrimesUnder(max, primeCache);
 
 console.log(primeCache.join('\t'));
